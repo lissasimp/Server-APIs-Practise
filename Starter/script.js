42ab1f288c2beeadf8f2af0578178f39965871e2
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-01-31 13:53:47 +0000

    Buttons added but not stored to local storage yet

--- Starter/script.js
+++ Starter/script.js
@@ -1,10 +1,51 @@
 //do I need to create an empty array of cities?
-var cities = [ ];
+var cities = [];
+
 
 //create a function to display weather info for our chosen city
-    function displayWeatherInfo(event) {}
+function displayWeatherInfo(event) {}
 
+//function for displaying city data
+function renderButtons() {
 
+// Deleting the city buttons prior to adding new city buttons
+  // (this is necessary otherwise we will have repeat buttons)
+  $("#today").empty(); //not sure if this is correct?
+
+  // Looping through the array of cities
+  for (var i = 0; i < cities.length; i++) {
+
+// Then dynamicaly generating buttons for each city in the array.
+    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class
+    a.addClass("city");
+    // Adding a data-attribute with a value of the city at index i
+    a.attr("data-name", cities[i]);
+    // Providing the button's text with a value of the city at index i
+    a.text(cities[i]);
+    // Adding the button to the HTML
+    $("#today").append(a);
+  }
+}
+// This function handles events where one button is clicked
+$("#search-button").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+  
+    // This line will grab the text from the input box
+    var city = $("#search-input").val().trim();
+    // The city from the textbox is then added to our array
+    cities.push(city);
+  
+    // calling renderButtons which handles the processing of our cities array
+    renderButtons();
+  });
+  
+  // Calling the renderButtons function at least once to display the initial list of cities
+  renderButtons();
+  
 // This is our API key - USE a prompt here for user to input?
 var apiKey = "76dd56a7c869514402bbcfd7dbd7cbb7";
 // var city = $ (this).attr("data-name");
@@ -18,18 +59,17 @@ $("#search-button").on("click", function(event) {
 
 event.preventDefault();
 
-//Get text from input box
-var city = $("search-input").val();
+ // This line will grab the text from the input box
+ var city = $("#search-input").val().trim();
 
-//Construct our query url
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
 
 //create an ajax for search button clicked
 $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=76dd56a7c869514402bbcfd7dbd7cbb7",
-    method: "GET"
+    url: queryURL,
+    method: "GET",
     }).then(function(response) {  // We store all of the retrieved data inside of an object called "response"
-        $("#today").text(JSON.stringify(response));
+        // $("#today").text(JSON.stringify(response));
     });
         
  // Log the queryURL
