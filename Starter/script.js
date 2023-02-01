b6f114cb6c235e83e5f4384b15b6d4537faf0559
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-02-01 14:15:35 +0000

    retrieving 5 day forecast

--- Starter/script.js
+++ Starter/script.js
@@ -8,63 +8,78 @@ var city = $("#search-input").val().trim();
 // This is our API key - USE a prompt here for user to input?
 var apiKey = "76dd56a7c869514402bbcfd7dbd7cbb7";
   // var city = $ (this).attr("data-name");
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
+var current = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
+var forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=" + apiKey;
 
-//create an ajax for search button clicked
+//create an ajax for search button clicked - displays current data
 $.ajax({
-    url: queryURL,
+    url: current,
     method: "GET",
     }).then(function(response) {  // We store all of the retrieved data inside of an object called "response"
+       
         // 1. Creating a div to hold the city
      var cityDiv = $("<div class='city'>");
 
-     // 2. Storing the temperature data
+     //2. Storing the name data
+     var cityName = response.name; //need today's date
+     console.log(cityName);
+
+     //creating an element to have the name displayed
+     var temp = $("<h1>").text(cityName);
+
+     // 3. Storing the temperature data
      var temperature = response.main.temp;
-     console.log(temperature)
+     console.log("Temperature: " + temperature + "°C")
 
      // Creating an element to have the temperature displayed
-     var temp = $("<p>").text("Temerature: " + temperature);
+     var temp = $("<p>").text("Temperature: " + temperature + "°C");
 
      // Displaying the temperature
-     cityDiv.append(temp);
+     $(".city").append(temp);
 
-     // 3. Storing the wind speed
+     // 4. Storing the wind speed
      var windSpeed = response.wind.speed;
-     console.log(windSpeed)
+     console.log("Wind Speed: " + windSpeed + "KPH")
 
      // Creating an element to hold the wind speed
-     var wind = $("<p>").text("Wind Speed: " + windSpeed);
+     var wind = $("<p>").text("Wind Speed: " + windSpeed + "KPH");
 
      // Displaying the wind speed
-     cityDiv.append(wind);
+     $(".city").append(wind);
 
-     // 4. Storing the humidity
+     // 5. Storing the humidity
      var humidity = response.main.humidity;
-     console.log(humidity)
+     console.log("Humidity: " + humidity + "%")
 
      // Creating an element to hold the wind speed
-     var humid = $("<p>").text("Humidity: " + humidity);
+     var humid = $("<p>").text("Humidity: " + humidity + "%");
 
      // Displaying the wind speed
      cityDiv.append(humid);
     });
-
+//create data for 5 day weather forecast
+$.ajax({
+    url: forecast,
+    method: "GET",
+    }).then(function(response) {  // We store all of the retrieved data inside of an object called "response"
+        console.log(forecast)
+    });
 }
 //function for displaying city data
 function renderButtons() {
 
     // Deleting the city buttons prior to adding new city buttons
       // (this is necessary otherwise we will have repeat buttons)
-      $("#today").empty(); //not sure if this is correct?
+      $("#today").empty(); 
     
       // Looping through the array of cities
       for (var i = 0; i < cities.length; i++) {
     
     // Then dynamicaly generating buttons for each city in the array.
         // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var a = $("<button>");
+        var a = $("<cityButton>");
         // Adding a class
-        a.addClass("city");
+        a.addClass("pastCity");
         // Adding a data-attribute with a value of the city at index i
         a.attr("data-name", cities[i]);
         // Providing the button's text with a value of the city at index i
